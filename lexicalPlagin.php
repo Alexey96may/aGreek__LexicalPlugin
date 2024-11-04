74d18744e26ef7299c7fd0c1b03a8c637839dbf6
Alexey96may <shulga_alexey@vk.com>
2024-11-04 20:08:14 +0300

    First real Commit

--- lexicalPlagin.php
+++ lexicalPlagin.php
@@ -1,1 +1,14 @@
+<?php
 
+/*
+  Plugin Name: Lexical Trainer
+  Description: The Lexical Trainer allows creating of trainers to improve the lexical level.
+  Author: Alexey Shulga
+  Version: 0.0.1
+  Author URI: https://github.com/Alexey96may
+  License: GPLv2 or later
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; //Exit if accessed directly
+}
