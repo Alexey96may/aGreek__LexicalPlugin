f50b4cf988411e108f7b6531ab626738acd0f4c6
Alexey96may <shulga_alexey@vk.com>
2024-11-05 19:22:00 +0300

    checking up the relationt between the site and the plugin

--- lexicalPlagin.php
+++ lexicalPlagin.php
@@ -12,3 +12,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; //Exit if accessed directly
 }
+
+class LexicalPlugin {
+  function __construct($string){
+    echo "Hello $string";
+  }
+}
+
+if (class_exists(LexicalPlugin)) {
+  $plugin = new LexicalPlugin("user");
+} else {
+  die;
+}
